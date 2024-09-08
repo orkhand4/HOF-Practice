898412844e3b3d603d09f555604d6f52f2de731f
Orkhand4 <orkhand4@mail.ru>
2024-09-08 10:47:50 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const workers = [
+  {
+  fullName: 'Zahid Vahabzade',
+  isMarry: true,
+  salary: 7000,
+  speciality: 'Full Stack Dev'
+  },
+  {
+  fullName: 'Fuad Mikayilzade',
+  isMarry: false,
+  salary: 5000,
+  speciality:  'DevOps'
+  },
+  {
+  fullName: 'Zahid Vahabzade',
+  isMarry: true,
+  salary: 3000,
+  speciality: 'Backend Dev'
+  }
+];
+
+workers.map(item=>console.log(item.salary)
+)
+
