2580ef332331f18c35d05109bbddb1a1020015d3
Orkhand4 <orkhand4@mail.ru>
2024-09-08 10:58:26 +0400

    ID

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const workers = [
   {
-  fullName: 'Zahid Vahabzade',
-  isMarry: true,
-  salary: 7000,
-  speciality: 'Full Stack Dev'
+    id: 1,
+    fullName: "Zahid Vahabzade",
+    isMarry: true,
+    salary: 7000,
+    speciality: "Full Stack Dev",
   },
   {
-  fullName: 'Fuad Mikayilzade',
-  isMarry: false,
-  salary: 5000,
-  speciality:  'DevOps'
+    id: 2,
+    fullName: "Fuad Mikayilzade",
+    isMarry: false,
+    salary: 5000,
+    speciality: "DevOps",
   },
   {
-  fullName: 'Zahid Vahabzade',
-  isMarry: true,
-  salary: 3000,
-  speciality: 'Backend Dev'
-  }
+    id: 3,
+    fullName: "Orkhan Aliyev",
+    isMarry: true,
+    salary: 3000,
+    speciality: "Backend Dev",
+  },
 ];
 
-const richWorkers = workers.filter(item=>item.salary>4000)
-console.log(richWorkers);
+// const richWorkers = workers.filter((item) => item.salary > 4000);
+// console.log(richWorkers);
 
+const ogru = workers.find(worker => worker.id == 3)
+console.log(`Ogru: ${ogru.fullName}`);
 
